6ddc79d8d66a0c8750a104d89035322d8225b18a
Travs <79965637+TravsDev@users.noreply.github.com>
2022-01-21 10:25:14 +0100

    Add files via upload

---
+++ Funny_Matrix.bat
@@ -0,0 +1,24 @@
+@echo off
+
+title Hack
+
+:1
+color 3
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto 2
+
+:2
+color c
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto 3
+
+:3
+color 6
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+goto 4
+
+:4
+color f
+echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
+
+goto 1
